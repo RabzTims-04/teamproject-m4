1b77cd52d3a365c06aa15c4685ce693a9605998c
Kristian Utsar <78056935+Utsar@users.noreply.github.com>
2021-06-04 14:41:40 +0100

    last

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -28,8 +28,8 @@ class MyNav extends Component {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent" />
         <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link active" href="home.html">
+          <li className="nav-item">
+            <a className="nav-link" href="home.html">
               Home <span className="sr-only">(current)</span>
             </a>
           </li>
@@ -54,22 +54,28 @@ class MyNav extends Component {
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="./backoffice.html">
+            <a className="nav-link" href="#">
               Form
             </a>
           </li>
         </ul>
         <div className="d-flex align-items-center" />
         <a
-          className="nav-link disabled"
+          class="nav-link disabled"
           href="#"
           tabindex="-1"
           aria-disabled="true"
         >
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="white"
+            class="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
         </a>
         <a href="#">
           <svg
